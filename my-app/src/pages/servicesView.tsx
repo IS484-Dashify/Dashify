f9a8f9ed76480029b991392dd168fcb25a5764c7
Michael Ong <michaelong.2021@scis.smu.edu.sg>
2024-01-30 23:51:41 +0800

    Installed nextUI

--- my-app/src/pages/servicesView.tsx
+++ my-app/src/pages/servicesView.tsx
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import React from 'react';
-import SearchBar from './components/searchBar'
-import { Breadcrumb, Card, Avatar} from 'antd';
-import { LoginOutlined, CreditCardOutlined, NotificationOutlined, SearchOutlined, EnvironmentOutlined} from '@ant-design/icons';
+// import SearchBar from './components/searchBar'
+// import { Breadcrumb, Card, Avatar} from 'antd';
+// import { LoginOutlined, CreditCardOutlined, NotificationOutlined, SearchOutlined, EnvironmentOutlined} from '@ant-design/icons';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,25 +12,25 @@ interface serviceItem {
   Icon: React.ComponentType<any>
 }
 
-const serviceList = {
-  "Geolocation" : ["green", EnvironmentOutlined],
-  "Login": ["red", LoginOutlined],
-  "Payment": ["amber", CreditCardOutlined],
-  "Notification": ["green", NotificationOutlined],
-  "Search": ["green", SearchOutlined],
-}
+// const serviceList = {
+//   "Geolocation" : ["green", EnvironmentOutlined],
+//   "Login": ["red", LoginOutlined],
+//   "Payment": ["amber", CreditCardOutlined],
+//   "Notification": ["green", NotificationOutlined],
+//   "Search": ["green", SearchOutlined],
+// }
 
-// sorting functions be changed depending on the serviceList
-const sortedServiceArray: serviceItem[] = Object.entries(serviceList).map(([serviceName, [status, Icon]]) => ({
-  serviceName,
-  status: status as string,
-  Icon: Icon as React.ComponentType<any>
-}));
+// // sorting functions be changed depending on the serviceList
+// const sortedServiceArray: serviceItem[] = Object.entries(serviceList).map(([serviceName, [status, Icon]]) => ({
+//   serviceName,
+//   status: status as string,
+//   Icon: Icon as React.ComponentType<any>
+// }));
 
-const order:{ [key: string]: number} = { red: 0, amber: 1, green: 2 };
-sortedServiceArray.sort((a, b) => {
-  return order[a.status] - order[b.status];
-});
+// const order:{ [key: string]: number} = { red: 0, amber: 1, green: 2 };
+// sortedServiceArray.sort((a, b) => {
+//   return order[a.status] - order[b.status];
+// });
 
 
 export default function Home() {
@@ -38,16 +38,16 @@ export default function Home() {
   return (
     <main>
       <div className="p-14">
-        <Breadcrumb
+        {/* <Breadcrumb
           className="text-xl mb-10"
           items={[
             {
               title: "Services",
             },
           ]}
-        />
-        <SearchBar/>
-        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
+        /> */}
+        {/* <SearchBar/> */}
+        {/* <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
           {sortedServiceArray.map(({ serviceName, status, Icon }) => (
             <Card 
               key={serviceName} 
@@ -72,7 +72,7 @@ export default function Home() {
               <span className="ml-10">{serviceName}</span>
             </Card>
           ))}
-        </div>
+        </div> */}
       </div>
     </main>
   );
