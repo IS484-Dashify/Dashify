914c644402c251c1705ac278a95354f9ec349ef5
tianwanlin <105766416+tianwanlin@users.noreply.github.com>
2024-02-04 19:48:41 +0800

    map with markers and tooltip

---
+++ my-app/src/pages/worldView.tsx
@@ -0,0 +1,151 @@
+import { Inter } from "next/font/google";
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogin , AiOutlineCreditCard, AiOutlineNotification, AiOutlineSearch, AiOutlineEnvironment, AiOutlineHome, AiOutlineBars} from 'react-icons/ai';
+import { FaCircle } from "react-icons/fa"
+import {Breadcrumbs, BreadcrumbItem, Tooltip} from "@nextui-org/react";
+import { useRouter } from 'next/router';
+import { ComposableMap, Geographies, Geography, Marker, Annotation, ZoomableGroup } from "react-simple-maps"
+import Map from "../../public/map.json"
+import { hasFlag, countries } from 'country-flag-icons'
+import "country-flag-icons/3x2/flags.css";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+interface serviceItem {
+  serviceName: string,
+  status: string,
+  Icon: React.ComponentType<any>
+}
+
+// sorting functions be changed depending on the mockServices
+const mockServices = [
+  {
+    serviceName: "Login",
+    status: "red",
+    Icon: AiOutlineLogin
+  },
+  {
+    serviceName: "Payment",
+    status: "amber",
+    Icon: AiOutlineCreditCard
+  },
+  {
+    serviceName: "Notification",
+    status: "green",
+    Icon: AiOutlineNotification
+  },
+  {
+    serviceName: "Search",
+    status: "green",
+    Icon: AiOutlineSearch
+  },
+  {
+    serviceName: "Geolocation",
+    status: "green",
+    Icon: AiOutlineEnvironment
+  },
+];
+
+const markers = [
+  { markerOffset: 15, name: "Singapore", iso: "SG", coordinates: [103.8198, 1.3521], status:"green", serviceNo: 3, red: 0, amber: 1, green: 3}, // 103.8198° E, 1.3521° N
+  { markerOffset: 15, name: "Australia", iso: "AU", coordinates: [133.281323, -26.4390917], status:"green", serviceNo: 3, red: 0, amber: 1, green: 3},
+  { markerOffset: 15, name: "China", iso: "CN", coordinates: [79.3871, 43.6426], status:"amber", serviceNo: 3, red: 0, amber: 1, green: 3 },
+  { markerOffset: 15, name: "France", iso: "FR", coordinates: [1.7191036, 46.2276], status:"amber", serviceNo: 3, red: 0, amber: 1, green: 3  },
+  { markerOffset: 15, name: "Hong Kong", iso: "HK", coordinates: [114.1694, 22.3193], status:"red", serviceNo: 3, red: 0, amber: 1, green: 3  },
+  { markerOffset: 15, name: "United States", iso: "US", coordinates: [-95.7129, 37.0902], status:"red", serviceNo: 3, red: 0, amber: 1, green: 3  }
+];
+
+const getIconForService = (serviceName) => {
+  const service = mockServices.find((s) => s.serviceName === serviceName);
+  const icon = service? service.Icon : null;
+  return icon ? React.createElement(icon) : '';
+};
+
+const tooltipContent = (countryName, iso, serviceNo, red, amber, green) => {
+
+  if (countries.includes(iso) === true && hasFlag(iso) === true){
+    return(
+      <div className="px-1 py-2 flex flex-row">
+        <div className="flex justify-center items-center px-3">
+          <span className={"flag:" + iso} />
+        </div>
+        <div>
+          <div className="text-md font-bold mb-3">{countryName}</div>
+          <div className="text-sm">
+            <span className="inline-flex items-center me-2"><AiOutlineBars className="me-1"/> {serviceNo}</span>
+            <span className="inline-flex items-center me-2"><FaCircle className="text-reddish-200 me-1"/> {red} </span>
+            <span className="inline-flex items-center me-2"><FaCircle className="text-amberish-200 me-1"/> {amber}</span>
+            <span className="inline-flex items-center me-2"><FaCircle className="text-greenish-200 me-1"/> {green}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default function WorldView() {
+  const [currentPage, setCurrentPage] = React.useState("world");
+  const router = useRouter();
+  const { service } = router.query;
+
+  return (
+    <main>
+      <div className="h-screen min-h-full px-14 pt-6">
+        <div id='top-menu'>
+          <Breadcrumbs 
+            size="lg" 
+            underline="hover" 
+            // classNames={{
+            //   list: "bg-stone-200",
+            // }}
+            // variant="solid" 
+            onAction={(key) => setCurrentPage(String(key))}
+          >
+            <BreadcrumbItem key="services" href="/servicesView" startContent={<AiOutlineHome/>}>
+              Services
+            </BreadcrumbItem>
+            <BreadcrumbItem key="world" href="/worldView" startContent={getIconForService(service)} isCurrent={currentPage === "world"}>
+              {service}
+            </BreadcrumbItem>
+          </Breadcrumbs>
+          {/* <h1 className='text-4xl font-bold text-indigo-d-500 mt-1 pb-8 pt-2'>{service}</h1> */}
+          <h1 className='text-4xl font-bold text-indigo-d-500 mt-1 pt-2'>Login</h1>
+        </div>
+        <div data-tip="" className="flex justify-center">
+          <ComposableMap className="w-7/12">
+            <Geographies geography={Map} fill="#e2dbf7" stroke="#a793e8">
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography 
+                    key={geo.rsmKey} 
+                    geography={geo} 
+                  />
+                ))
+              }
+            </Geographies>
+            {markers.map(({ name, iso, coordinates, markerOffset, status, serviceNo, red, amber, green }) => (
+              <Marker key={iso} coordinates={coordinates}>
+                 {
+                    status === "red" 
+                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
+                        <circle r={10} fill="#ffa5a1" stroke="#f01e2c" strokeWidth={2} />
+                      </Tooltip>
+                    ): status === "amber"
+                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
+                        <circle r={10} fill="#ffc17a" stroke="#ff7e00" strokeWidth={2} />
+                      </Tooltip>
+                    ): status === "green"
+                    ?  (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
+                        <circle r={10} fill="#acdf87" stroke="#4c9a2a" strokeWidth={2} />
+                      </Tooltip>
+                    ): null
+                  }
+              </Marker>
+            ))}
+          </ComposableMap>
+        </div>
+      </div>
+    </main>
+  );
+}
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
 
+const {nextui} = require("@nextui-org/react");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -13,8 +16,59 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "p-white": "#F2F3F4", // ! Background colour
+        "indigo-d": {
+          50: "#E7E9F7",
+          100: "#e2dbf7",
+          200: "#c4b7ef",
+          300: "#a793e8",
+          400: "#896fe0",
+          500: "#6c4bd8", // ! main primary coloor
+          600: "#563cad",
+          700: "#412d82",
+          800: "#2b1e56",
+          900: "#160f2b"
+        },
+        "text": "#060828",
+        "lavender": {
+          100: "#ede9f3",
+          200: "#dad3e8",
+          300: "#c8bddc",
+          400: "#b5a7d1",
+          500: "#a391c5", // ! main secondary colour
+          600: "#82749e",
+          700: "#625776",
+          800: "#413a4f",
+          900: "#211d27"
+        },
+        "accent": {
+          100: "#d1d3f0",
+          200: "#a3a8e1",
+          300: "#747cd2",
+          400: "#4651c3",
+          500: "#1825b4", // ! main accent colour
+          600: "#131e90",
+          700: "#0e166c",
+          800: "#0a0f48",
+          900: "#050724"
+        },
+        "reddish": {
+          100: "#ffa5a1",
+          200: "#f01e2c"
+        },
+        "amberish": {
+          100: "#ffc17a",
+          200: "#ff7e00"
+        },
+        "greenish": {
+          100: "#acdf87",
+          200: "#4c9a2a"
+        }
+      }
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
