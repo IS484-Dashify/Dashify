62aae97737cf350c21a1b9e37a7d12ea0befa546
MikeyTheOng <michaelong.2021@scis.smu.edu.sg>
2024-02-13 06:03:03 +0800

    Updated nextauth config + created api route to check if email exists in database

--- my-app/src/pages/api/auth/[...nextauth].js
+++ my-app/src/pages/api/auth/[...nextauth].js
@@ -10,6 +10,42 @@ export const authOptions = {
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        async signIn ({ user, account, profile }) {
+            // console.log("signin callback [user]", {user});
+            // console.log("signin callback [account]", {account});
+            // console.log("signin callback [profile]", {profile});
+            // console.log("Verified: ", profile.email_verified);
+
+            if (profile.email_verified) {
+                try {
+                    // 1. Check if email exists in database
+                    // console.log("EMAIL", `/api/user-auth/${user.email}`);
+                    const response = await fetch(`http://localhost:3000/api/user-auth/${user.email}`);
+                    const data = await response.json();
+                    // console.log("DATA", data);
+                    // return true;
+                    // // 2. If email exists, return true
+                    if (response.status === 200) {
+                        console.log("user_exists:", data);
+                        return true
+                    } else {
+                        // 3. If email does not exist, return false
+                        console.log("user_does_not_exist:", data);
+                        return false
+                    }
+                } catch (error) {
+                    console.error('Fetch error:', error);
+                    return false;
+                }
+            } else {
+                // Return false to display a default error message
+                return false
+                // Or you can return a URL to redirect to:
+                // return '/unauthorized'
+            }
+        }
+    }
 }
 
 export default NextAuth(authOptions)
---
+++ my-app/src/pages/api/user-auth/[email].ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createConnection } from 'mysql2/promise';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const db = await createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: 'dashify',
+        port: 3306
+    });
+    const { query, method } = req;
+    const email = query.email;
+    if(method==='GET'){
+        try{           
+            const [results, fields] = await db.query(
+                'SELECT * FROM user where email = ?', [email]
+            );
+
+            if(results[0] === undefined){
+                res.status(400).json({error: 'User not found'});
+            } else {
+                res.status(200).json({ results: results[0] });
+            }
+        } catch (error) {
+            res.status(400).json({error: error})
+        } finally {
+            // Ensure the connection is closed whether or not there was an error
+            if (db) {
+                await db.end();
+            }
+        }
+    } 
+}
