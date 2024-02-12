bfc8414db5144b8a997787703ccdabe1411acc71
MikeyTheOng <michaelong.2021@scis.smu.edu.sg>
2024-02-13 00:29:26 +0800

    Installed NextAuth and implemented Authentication on test-login and login pages

--- my-app/src/pages/test-login.tsx
+++ my-app/src/pages/test-login.tsx
@@ -6,28 +6,29 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-export default function Home() {
+export default function TestLogin() {
   const { data: session } = useSession();
   useEffect(() => {
     console.log("Session:", session);
   }, [session]);
-
+    
   const router = useRouter();
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     router.push("/servicesView");
   }
   if(session){
-    return(
+    return (
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
         <button onClick={handleClick}>
           ServicesView
         </button>
-      </main>)
+      </main>
+    );
   } else {
-    return(
+    return (
       <>
         Not signed in <br />
         <button onClick={() => signIn()}>Sign in</button>
