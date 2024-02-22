dbfcad7f4c1686ebd2dc8b158df757f2c48e529f
tianwanlin <105766416+tianwanlin@users.noreply.github.com>
2024-02-22 22:21:19 +0800

    side pop up when country is clicked

--- my-app/src/pages/worldView.tsx
+++ my-app/src/pages/worldView.tsx
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
 import React, { useState, useEffect } from 'react';
-import { AiOutlineLogin , AiOutlineCreditCard, AiOutlineNotification, AiOutlineSearch, AiOutlineEnvironment, AiOutlineHome, AiOutlineBars} from 'react-icons/ai';
-import { FaCircle } from "react-icons/fa"
+import { AiOutlineLogin , AiOutlineCreditCard, AiOutlineNotification, AiOutlineSearch, AiOutlineEnvironment, AiOutlineHome, AiOutlineBars } from 'react-icons/ai';
+import { FaCircle } from "react-icons/fa";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md';
 import {Breadcrumbs, BreadcrumbItem, Tooltip} from "@nextui-org/react";
 import { useRouter } from 'next/router';
 import { ComposableMap, Geographies, Geography, Marker, Annotation, ZoomableGroup } from "react-simple-maps"
@@ -18,12 +20,72 @@ interface serviceItem {
   Icon: React.ComponentType<any>
 }
 
-// sorting functions be changed depending on the mockServices
 const mockServices = [
   {
     serviceName: "Login",
     status: "red",
-    Icon: AiOutlineLogin
+    Icon: AiOutlineLogin,
+    countries: [
+      { 
+        name: "Singapore", 
+        iso: "SG", 
+        coordinates: [103.8198, 1.3521], 
+        status:"green", 
+        vm: {
+          "VM1": "green",
+          "VM2": "green",
+          "VM3": "green"
+        }
+      }, // 103.8198° E, 1.3521° N
+      { 
+        name: "Australia", 
+        iso: "AU", 
+        coordinates: [133.281323, -26.4390917], 
+        status:"green", 
+        vm: {
+          "VM4": "green",
+        }
+      },
+      { 
+        name: "China", 
+        iso: "CN", 
+        coordinates: [79.3871, 43.6426], 
+        status:"amber", 
+        vm: {
+          "VM5": "green",
+          "VM6": "amber",
+        }
+      },
+      { 
+        name: "France", 
+        iso: "FR", 
+        coordinates: [1.7191036, 46.2276], 
+        status:"amber",
+        vm: {
+          "VM7": "amber",
+        }
+      },
+      { 
+        name: "Hong Kong", 
+        iso: "HK", 
+        coordinates: [114.1694, 22.3193], 
+        status:"red", 
+        vm: {
+          "VM8": "red",
+          "VM9": "amber",
+        }
+      },
+      { 
+        name: "United States", 
+        iso: "US", 
+        coordinates: [-95.7129, 37.0902], 
+        status:"red", 
+        vm: {
+          "VM10": "green",
+          "VM11": "red",
+        }
+      }
+    ]
   },
   {
     serviceName: "Payment",
@@ -47,14 +109,43 @@ const mockServices = [
   },
 ];
 
-const markers = [
-  { markerOffset: 15, name: "Singapore", iso: "SG", coordinates: [103.8198, 1.3521], status:"green", serviceNo: 3, red: 0, amber: 0, green: 3}, // 103.8198° E, 1.3521° N
-  { markerOffset: 15, name: "Australia", iso: "AU", coordinates: [133.281323, -26.4390917], status:"green", serviceNo: 3, red: 0, amber: 0, green: 3},
-  { markerOffset: 15, name: "China", iso: "CN", coordinates: [79.3871, 43.6426], status:"amber", serviceNo: 3, red: 0, amber: 1, green: 2 },
-  { markerOffset: 15, name: "France", iso: "FR", coordinates: [1.7191036, 46.2276], status:"amber", serviceNo: 3, red: 0, amber: 2, green: 0  },
-  { markerOffset: 15, name: "Hong Kong", iso: "HK", coordinates: [114.1694, 22.3193], status:"red", serviceNo: 3, red: 1, amber: 1, green: 1  },
-  { markerOffset: 15, name: "United States", iso: "US", coordinates: [-95.7129, 37.0902], status:"red", serviceNo: 4, red: 2, amber: 2, green: 0  }
-];
+const vmList = {
+  "VM1" : {
+    "component1": "green",
+    "component2": "green",
+    "component3": "green"
+  },
+  "VM2" : {
+    "component4": "green",
+  },
+  "VM3" : {
+    "component5": "green",
+  },
+  "VM4" : {
+    "component6": "green",
+  },
+  "VM5" : {
+    "component7": "green",
+  },
+  "VM6" : {
+    "component8": "amber",
+  },
+  "VM7" : {
+    "component9": "amber",
+  },
+  "VM8" : {
+    "component10": "red",
+  },
+  "VM9" : {
+    "component10": "amber",
+  },
+  "VM10" : {
+    "component10": "green",
+  },
+  "VM11" : {
+    "component10": "red",
+  }
+}
 
 const getIconForService = (serviceName) => {
   const service = mockServices.find((s) => s.serviceName === serviceName);
@@ -62,39 +153,127 @@ const getIconForService = (serviceName) => {
   return icon ? React.createElement(icon) : '';
 };
 
-const tooltipContent = (countryName, iso, serviceNo, red, amber, green) => {
+const tooltipContent = (countryName, iso, vm) => {
+  const status_counts = {"green": 0, "red": 0, "amber": 0};
+  if (countries.includes(iso) && hasFlag(iso)) {
+    Object.values(vm).forEach(machineStatus => {
+      if (machineStatus === "green") {
+        status_counts["green"] += 1;
+      } else if (machineStatus === "amber") {
+        status_counts["amber"] += 1;
+      } else {
+        status_counts["red"] += 1;
+      }
+    });
 
-  if (countries.includes(iso) === true && hasFlag(iso) === true){
     return(
-      <div className="px-1 py-2 ">
+      <div className="px-1 py-2">
         <span className={"flag:" + iso} /><div className="text-md font-bold mb-2 ml-2 inline-flex">{countryName}</div>
         <div className="text-sm">
-          <span className="inline-flex items-center me-2"><AiOutlineBars className="me-1"/> {serviceNo}</span>
-          <span className="inline-flex items-center me-2"><FaCircle className="text-reddish-200 me-1"/> {red} </span>
-          <span className="inline-flex items-center me-2"><FaCircle className="text-amberish-200 me-1"/> {amber}</span>
-          <span className="inline-flex items-center me-2"><FaCircle className="text-greenish-200 me-1"/> {green}</span>
+          <span className="inline-flex items-center me-2"><AiOutlineBars className="me-1"/> {Object.keys(vm).length}</span>
+          <span className="inline-flex items-center me-2"><FaCircle className="text-reddish-200 me-1"/> {status_counts["red"]}</span>
+          <span className="inline-flex items-center me-2"><FaCircle className="text-amberish-200 me-1"/> {status_counts["amber"]}</span>
+          <span className="inline-flex items-center me-2"><FaCircle className="text-greenish-200 me-1"/> {status_counts["green"]}</span>
         </div>
       </div>
     )
   }
 }
 
+const statusColors = {
+  green: "text-greenish-200 me-1",
+  amber: "text-amberish-200 me-1",
+  red: "text-reddish-200 me-1"
+};
+
+const ToggleableList = ({ items, vmName, status }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="border-b-2 py-3">
+      <div className="flex justify-between items-center">
+        <button className="flex items-center space-x-2 text-lg font-semibold" onClick={() => setIsOpen(!isOpen)}>
+          <span>{vmName}</span>
+          {isOpen ? <MdArrowDropUp /> : <MdArrowDropDown />}
+        </button>
+        <FaCircle className={statusColors[status]} />
+      </div>
+      {isOpen && (
+        <div className="mt-3">
+          {items.map((item, index) => (
+            <div className="pb-1">
+              <div key={index} className="flex items-center justify-between">
+                <span>{item.componentName}</span>
+                <FaCircle className={statusColors[item.status]} />
+              </div>
+              {(item.status === 'red' || item.status === 'amber') && (
+                <div className=" flex justify-between text-xs italic mt-1">
+                  <span>CPU down</span>
+                  <span >3 hours ago</span> 
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+// Assuming selectedMarker.vm is structured correctly and contains the VM identifiers you're interested in
+const RightPopup = ({ isOpen, setIsOpen, selectedMarker }) => {
+  if (!isOpen) return null;
+
+  const vmsToShow = Object.keys(selectedMarker.vm)
+    .filter(machine => Object.keys(vmList).includes(machine))
+    .map(machine => ({
+      name: machine,
+      status: selectedMarker.vm[machine],
+      components: Object.entries(vmList[machine]).map(([componentName, status]) => ({
+        componentName,
+        status
+      }))
+    }));
+
+  return (
+    <div className={`fixed right-0 top-0 w-72 p-6 h-full bg-gray-100 shadow-lg z-50`}>
+      <div className="flex flex-row items-center mb-6">
+        <button onClick={() => setIsOpen(false)}>
+          <IoArrowBackCircleOutline size="25px"/>
+        </button>
+        <div className="flex items-center pl-12">
+          <h2 className="font-bold text-lg">{selectedMarker.name}</h2>
+        </div>
+      </div>
+      {vmsToShow.map(vm => 
+        <ToggleableList key={vm.name} items={vm.components} vmName={vm.name} status={vm.status} />
+      )}
+    </div>
+  );
+};
+
+
+
 export default function WorldView() {
   const [currentPage, setCurrentPage] = React.useState("world");
   const router = useRouter();
   const { service } = router.query;
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [selectedMarker, setSelectedMarker] = useState(null);  
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarker(marker);
+    setIsPopupOpen(true);
+  };
 
   return (
     <main>
-      <div className="h-screen min-h-full px-14 pt-6">
+      <div className="h-screen min-h-full px-14 pt-6 h-dvh">
         <div id='top-menu'>
           <Breadcrumbs 
             size="lg" 
             underline="hover" 
-            // classNames={{
-            //   list: "bg-stone-200",
-            // }}
-            // variant="solid" 
             onAction={(key) => setCurrentPage(String(key))}
           >
             <BreadcrumbItem key="services" href="/servicesView" startContent={<AiOutlineHome/>}>
@@ -106,8 +285,8 @@ export default function WorldView() {
           </Breadcrumbs>
           <h1 className='text-4xl font-bold text-indigo-d-500 mt-1 pb-8 pt-2'>{service}</h1>
         </div>
-        <div data-tip="" className="flex justify-center">
-          <ComposableMap className="w-7/12">
+        <div className="flex justify-center">
+          <ComposableMap className="">
             <Geographies geography={Map} fill="#e2dbf7" stroke="#a793e8">
               {({ geographies }) =>
                 geographies.map((geo) => (
@@ -118,27 +297,29 @@ export default function WorldView() {
                 ))
               }
             </Geographies>
-            {markers.map(({ name, iso, coordinates, markerOffset, status, serviceNo, red, amber, green }) => (
-              <Marker key={iso} coordinates={coordinates}>
+            {mockServices.flatMap(service => service.countries?.map(({ name, iso, coordinates, status, vm }) => (
+              <Marker key={iso} coordinates={coordinates}  onClick={() => handleMarkerClick({ name, iso, coordinates, status, vm })}>
                  {
                     status === "red" 
-                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
-                        <circle r={10} fill="#ffa5a1" stroke="#f01e2c" strokeWidth={2} />
+                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, vm)}>
+                        <circle r={10} fill="#ffa5a1" stroke="#f01e2c" strokeWidth={2} onClick={() => handleMarkerClick({ name, iso, coordinates, status, vm })} />
                       </Tooltip>
                     ): status === "amber"
-                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
-                        <circle r={10} fill="#ffc17a" stroke="#ff7e00" strokeWidth={2} />
+                    ? (<Tooltip showArrow={true} content={tooltipContent(name, iso, vm)}>
+                        <circle r={10} fill="#ffc17a" stroke="#ff7e00" strokeWidth={2} onClick={() => handleMarkerClick({ name, iso, coordinates, status, vm })} />
                       </Tooltip>
                     ): status === "green"
-                    ?  (<Tooltip showArrow={true} content={tooltipContent(name, iso, serviceNo, red, amber, green )}>
-                        <circle r={10} fill="#acdf87" stroke="#4c9a2a" strokeWidth={2} />
+                    ?  (<Tooltip showArrow={true} content={tooltipContent(name, iso, vm)}>
+                        <circle r={10} fill="#acdf87" stroke="#4c9a2a" strokeWidth={2} onClick={() => handleMarkerClick({ name, iso, coordinates, status, vm })} />
                       </Tooltip>
                     ): null
                   }
               </Marker>
-            ))}
+              ))
+            )}
           </ComposableMap>
         </div>
+        <RightPopup isOpen={isPopupOpen} setIsOpen={setIsPopupOpen} selectedMarker={selectedMarker} />
       </div>
     </main>
   );
