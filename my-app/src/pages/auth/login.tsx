96eadadc64f52b732f0611b0d8ed8c34d46299ff
MikeyTheOng <michaelong.2021@scis.smu.edu.sg>
2024-02-27 00:45:54 +0800

    Touched up design of login page

--- my-app/src/pages/auth/login.tsx
+++ my-app/src/pages/auth/login.tsx
@@ -6,8 +6,8 @@ import type {
 import { signIn } from "next-auth/react"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]"
-import { Login } from  "../../../public/svgs"
 import { SlSocialGoogle } from "react-icons/sl";
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,23 +16,27 @@ export default function SignIn({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return(
     <main>
-      <div className="h-screen px-14 pt-6">
+      <div className="h-screen px-14">
         <div id="login-div" className="flex text-center justify-center items-center h-full">
-          <div className="flex justify-between items-end border rounded bg-lavender-200/50 px-6 py-12">
-            <div className="flex flex-col">
-              <h1 className='text-2xl font-bold text-indigo-d-500 mb-1'>Dashify</h1>
-              <p>Welcome back to Dashify!</p>
-              <div className="-mt-4 mb-4">
-                <div className="mx-auto">
-                  <Login/>
+          <div className="flex justify-between items-end px-24 py-12">
+            <div className="flex flex-col -mt-32">
+              {/* <h4 className='text-2xl text-text mb-1'>Welcome Back!</h4> */}
+              <div className="mt-4 mb-7">
+                <div className="flex justify-center align-center">
+                  <div>
+                    <Image src="/logo.png" alt="" width={150} height={150} />
+                  </div>
+                  <div className="my-auto ml-2">
+                    <h1 className='text-8xl italic text-indigo-d-500 mb-1'>Dashify</h1>
+                  </div>
                 </div>
               </div>
-              <div className="mx-auto">
+              <div className="mx-auto w-full">
                 <button
-                  className="h-[2.5rem] text-xl bg-indigo-d-500  px-4 text-[#F2F3F4] border-1 rounded border-indigo-d-500 shadow-md shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring flex items-center align-middle" 
+                  className="h-[2.5rem] w-full bg-indigo-d-500 text-[#F2F3F4] text-xl border-1 rounded border-indigo-d-500 shadow-md shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring flex items-center align-middle justify-center" 
                   onClick={() => signIn("google")}
                 >
-                  <SlSocialGoogle className="inline mr-2" size="20"/>
+                  <SlSocialGoogle className="inline mr-2" size="23"/>
                   Log In
                 </button>
               </div>
