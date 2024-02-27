d8f5968944fdcf0e0c4bcfb2763d170fd552dc7b
MikeyTheOng <michaelongningofficial@gmail.com>
2024-02-27 21:39:16 +0800

    Updated RagFilterMenu and serviceView's design with new colour scheme

--- my-app/src/pages/servicesView.tsx
+++ my-app/src/pages/servicesView.tsx
@@ -3,11 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from "next-auth/react";
 
-import { AiOutlineLogin , AiOutlineCreditCard, AiOutlineNotification, AiOutlineSearch, AiOutlineEnvironment, AiOutlineHome} from 'react-icons/ai';
-import { GiWorld } from "react-icons/gi";
+import { AiOutlineHome } from 'react-icons/ai';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 import RagFilterMenu from "./components/RagFilterMenu";
-import {Card, CardHeader} from "@nextui-org/react";
-import {Avatar} from "@nextui-org/react";
 import {Breadcrumbs, BreadcrumbItem} from "@nextui-org/react";
 import * as Label from '@radix-ui/react-label';
 import Link from 'next/link';
@@ -18,35 +16,29 @@ const inter = Inter({ subsets: ["latin"] });
 interface serviceItem {
   serviceName: string,
   status: string,
-  Icon: React.ComponentType<any>
 }
 
 // sorting functions be changed depending on the mockServices
 const mockServices = [
   {
     serviceName: "Login",
-    status: "red",
-    Icon: AiOutlineLogin
+    status: "red"
   },
   {
     serviceName: "Payment",
     status: "amber",
-    Icon: AiOutlineCreditCard
   },
   {
     serviceName: "Notification",
     status: "green",
-    Icon: AiOutlineNotification
   },
   {
     serviceName: "Search",
     status: "green",
-    Icon: AiOutlineSearch
   },
   {
     serviceName: "Geolocation",
     status: "green",
-    Icon: AiOutlineEnvironment
   },
 
 ];
@@ -129,7 +121,7 @@ export default function ServiceView() {
 
   return (
     <main>
-      <div className="h-screen min-h-full overflow-hidden flex flex-row">
+      <div className="h-screen min-h-full flex flex-row">
         <Sidebar/>
         <div className="w-full px-14 pt-6">
           <div id='top-menu' className='mb-8 z-50'>
@@ -142,9 +134,9 @@ export default function ServiceView() {
                 Services View
               </BreadcrumbItem>
             </Breadcrumbs>
-            <h1 className='text-4xl font-bold text-indigo-d-500 mt-1 pb-8 pt-2'>Services</h1>
+            <h1 className='text-4xl font-bold text-pri-500 mt-1 pb-8 pt-2'>Services</h1>
             <div className='flex justify-center items-center w-full'>
-              <div className="">
+              <div className="flex w-fit">
                 <Label.Root className="text-[15px] font-medium leading-[35px] text-text mr-2" htmlFor="">
                   Filter By
                 </Label.Root>
@@ -153,7 +145,7 @@ export default function ServiceView() {
               <div className="flex flex-wrap items-center gap-[15px] ml-2 mr-2.5">
                 <input
                   autoComplete="off"
-                  className="inline-flex h-[2.5rem] w-[34rem] appearance-none bg-transparent border-1 border-slate-500/20 shadow-inner items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-text placeholder:text-text/50 outline-none hover:placeholder:text-lavender-500 hover:bg-lavender-100/70 focus:shadow focus:bg-lavender-100/70 focus:border-lavender-500 focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent focus:ring-lavender-500 transition-all duration-200 ease-in-out"
+                  className="inline-flex h-[2.5rem] w-[34rem] appearance-none bg-transparent border-1 border-slate-500/20 shadow-inner items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-text placeholder:text-text/50 outline-none hover:placeholder:text-pri-500 hover:bg-pri-100/70 hover:border-pri-500 focus:shadow focus:bg-pri-100/70 focus:border-pri-500 focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent focus:ring-pri-500 transition-all duration-200 ease-in-out"
                   type="text"
                   id="searchQuery"
                   placeholder="Search services by name..."
@@ -163,7 +155,7 @@ export default function ServiceView() {
               </div>
               <div>
                 <button 
-                  className="h-[2.5rem] px-4 bg-indigo-d-300 rounded-[4px] text-[#F2F3F4] border-1 border-indigo-d-300 shadow-md shadow-transparent hover:border-indigo-d-400 hover:bg-indigo-d-400 hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring"
+                  className="h-[2.5rem] px-4 bg-pri-300 rounded-[4px] text-[#F2F3F4] border-1 border-pri-300 shadow-md shadow-transparent hover:border-pri-500 hover:bg-pri-500 hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring"
                   onClick={() => {handleReset()}}
                 >
                   Reset
@@ -172,37 +164,46 @@ export default function ServiceView() {
             </div>
           </div>
           {renderedServices.length > 0 ? (
-            <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
-              {renderedServices.map(({ serviceName, status, Icon }, index) => (
-                <div className="shadow-lg shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring rounded-lg" key={index}>
+            <div className="grid grid-cols-4 gap-7">
+              {renderedServices.map(({ serviceName, status }, index) => (
+                <div className={`py-3 px-6 cursor-pointer z-0 border-l-4 rounded-sm bg-white shadow-lg shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring ${status === "red" ? "border-reddish-200" : status === "green" ? "border-greenish-200" : "border-amberish-200"}`} key={index}>
+
+                
                   <Link href={`/worldView?service=${serviceName}`}>
-                    <Card 
+                    <div id="serviceCard" className="flex justify-between">
+                      <h4 className="font-bold text-md text-text">{serviceName}</h4>
+                      <div>
+                        <MdOutlineArrowForwardIos 
+                          size={20} 
+                          className="text-text/30 h-full"
+                        />
+                      </div>
+                    </div>
+                    {/* <Card 
                       className='py-2 px-4 cursor-pointer z-0 bg-white shadow-none rounded-lg'
                     >
                       <CardHeader className="flex justify-start align-middle text-text">
                         {
                           status === "red" 
                           ? (<Avatar  
-                              icon={<Icon size={24}/>} 
+                              icon
                               style={{ backgroundColor: "#ffa5a1", color: "#f01e2c"}}
                             />
                           ): status === "amber"
                           ? (<Avatar  
-                              icon={<Icon size={24}/>} 
+                              icon
                               style={{ backgroundColor: '#ffc17a', color: "#ff7e00"}}
                             />
                           ): status === "green"
                           ?  (<Avatar  
-                              icon={<Icon size={24}/>} 
+                              icon
                               style={{ backgroundColor: "#acdf87", color: "#4c9a2a" }}
                             />
                           ): null
                         }
                         <h4 className="font-bold text-large text-text ml-4">{serviceName}</h4>
-                        {/* <p className="text-tiny uppercase font-bold">Daily Mix</p>
-                        <small className="text-default-500">12 Tracks</small> */}
                       </CardHeader> 
-                    </Card>
+                    </Card> */}
                   </Link>
                 </div>
               ))}
