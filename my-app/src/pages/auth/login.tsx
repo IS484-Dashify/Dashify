e8d3830394cc8e5719e90f6f77ddb020fb25e9af
MikeyTheOng <michaelongningofficial@gmail.com>
2024-02-27 23:05:22 +0800

    Merge with redesign branch and updated login page to use primary colours

--- my-app/src/pages/auth/login.tsx
+++ my-app/src/pages/auth/login.tsx
@@ -19,7 +19,7 @@ export default function SignIn({
     <main>
       <div className="h-screen px-14 pt-6">
         <div id="login-div" className="flex flex-col text-center justify-center items-center h-full">
-          <div className="flex items-start justify-center rounded-t-xl bg-blue-500 w-2/5 h-[40%] px-12 pt-12 relative">
+          <div className="flex items-start justify-center rounded-t-xl bg-pri-500 w-2/5 h-[40%] px-12 pt-12 relative">
             <Image src="/logo(white).png" alt="" width={400} height={400} />
             
           </div>
@@ -30,7 +30,7 @@ export default function SignIn({
                 <p className="mb-6 text-lg font-bold">Welcome back!</p>
                 <div className="flex justify-center">
                   <button
-                    className="h-[2.5rem] text-xl bg-blue-500 px-4 text-[#F2F3F4] border-1 rounded border-blue-500 shadow-md shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring flex items-center align-middle" 
+                    className="h-[2.5rem] text-xl bg-pri-500 px-4 text-[#F2F3F4] border-1 rounded border-pri-500 shadow-md shadow-transparent hover:shadow-slate-500/45 transition-all duration-300 ease-soft-spring flex items-center align-middle" 
                     onClick={() => signIn("google")}
                   >
                     <SlSocialGoogle className="inline mr-2" size="20"/>
