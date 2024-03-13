4a6e8884c1b9ca1e7c86e5bdfefdfbd7ad7fbb2d
MikeyTheOng <michaelongningofficial@gmail.com>
2024-03-14 00:37:48 +0800

    Created service.ts api route to retrieve all services from backend

---
+++ my-app/src/pages/api/services.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+interface Service {
+  name: string;
+  sid: number;
+}
+
+type SuccessResponse = {
+  services: Service;
+};
+
+interface ErrorResponse {
+  error: object;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<SuccessResponse | ErrorResponse>,
+) {
+  await fetch('http://127.0.0.1:5001/get-all-services')
+  .then(response => response.json())
+  .then(data => {
+    console.log("data:", data)
+    res.status(200).json({ services: data });
+  })
+  .catch(error => {
+    console.log("error:", error)
+    res.status(500).json({ error: error });
+  })
+
+  
+}
