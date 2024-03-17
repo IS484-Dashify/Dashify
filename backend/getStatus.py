367ca00ba43906079350e8010082306f46d997cd
MikeyTheOng <michaelong.2021@scis.smu.edu.sg>
2024-03-18 06:11:49 +0800

    Updated getStatus.py and services.py to log time

--- backend/getStatus.py
+++ backend/getStatus.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request
 import requests
 from app import app
+import time
 
 @app.route('/get-service-status-details/<int:sid>', methods=['GET'])
 def get_all_service_name_and_status(sid):
@@ -42,25 +43,37 @@ def get_all_service_name_and_status(sid):
 
 @app.route('/get-all-service-name-and-status', methods=['GET'])
 def get_service_status_details():
+    overall_start_time = time.time()
     output = {}
-
+    service_start_time = time.time()
     response1 = requests.get('http://localhost:5001/get-all-services')
     services = response1.json()['results']
+    service_end_time = time.time()
+    app.logger.info(f"Service Request took {service_end_time - service_start_time:.6f} seconds")
 
     for service in services:
         sid = service['sid']
+        machine_start_time = time.time()
         response2 = requests.get(f'http://localhost:5002/get-mid-by-sid/{sid}')
         mids = response2.json()['results']
+        machine_end_time = time.time()
+        app.logger.info(f"Machine Request took {machine_end_time - machine_start_time:.6f} seconds")
 
         for mid in mids:
             statuses = []
+            component_start_time = time.time()
             response3 = requests.get(f'http://localhost:5003/get-cid-by-mid/{mid}')
             cids = response3.json()['results']
+            component_end_time = time.time()
+            app.logger.info(f"Component Request took {component_end_time - component_start_time:.6f} seconds")
 
             for cid in cids:
+                results_start_time = time.time()
                 response4 = requests.get(f'http://localhost:5004/get-result-status/{cid}/{mid}')
                 component_status = response4.json()['status']
                 statuses.append(component_status)
+                results_end_time = time.time()
+                app.logger.info(f"Result Request took {results_end_time - results_start_time:.6f} seconds")
         
         if 'red' in statuses:
             output[service['name']] = 'Critical'
@@ -73,6 +86,8 @@ def get_service_status_details():
 
 
     # Return the final response
+    overall_end_time = time.time()
+    app.logger.info(f"Overall Request took {overall_end_time - overall_start_time:.6f} seconds")
     return jsonify(output)
 
 
--- backend/services.py
+++ backend/services.py
@@ -1,12 +1,16 @@
 from flask import jsonify
 from models import Services
 from app import app
+import time
 
 @app.route('/get-all-services', methods=['GET'])
 def get_all_services():
+    overall_start_time = time.time()
     all_services = Services.query.all()
     services = [service.json() for service in all_services]
     print(services)
+    overall_end_time = time.time()
+    app.logger.info(f"Overall Request took {overall_end_time - overall_start_time:.6f} seconds")
     return jsonify({'results': services})
 
 if __name__ == '__main__':
