f24268d8a67889af94d9f7d11235533380d23621
lionelgwk <lionelgwk@icloud.com>
2024-03-17 14:38:22 +0800

    solving double db initialisation

--- backend/app.py
+++ backend/app.py
@@ -13,11 +13,12 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # db = SQLAlchemy(app)
+
+    db.init_app(app)
     CORS(app)
     return app
 
 app = create_app()
-db = SQLAlchemy(app)  # Initialize SQLAlchemy outside of create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
