77fb5f2122238ac6512da06a301c7ad4f73f09c5
lionelgwk <lionelgwk@icloud.com>
2024-03-22 00:13:22 +0800

    component creation adds threshold and setup env

--- backend/components.py
+++ backend/components.py
@@ -1,6 +1,10 @@
-from flask import jsonify
+from flask import jsonify, request
 from models import Components
 from app import app, db
+import requests
+from dotenv import load_env()
+from os import environ
+load_env()
 
 @app.route('/get-all-components', methods=['GET'])
 def get_all_components():
@@ -18,6 +22,50 @@ def get_cid_values_by_mid(mid):
     components = Components.query.filter_by(mid=mid).all()
     cids = [component.cid for component in components]
     return jsonify({"results": cids})
+
+@app.route('/add-component', methods=['POST'])
+def add_component():
+    data = request.get_json()
+    component = Components(**data)
+    db.session.add(component)
+    db.session.commit()
+    try:
+        requests.post(environ.get('createThresholdURL'), json={
+            "cid": data.get('cid'),
+            "Warning": 80,
+            "Critical": 90
+        })
+    except Exception as e:
+        return jsonify({'error': 'There was an error creating the new component\'s threshold: ' + str(e)})
+
+    try:
+        requests.post(environ.get('setupEnvURL'), json={
+            "cid": data.get('cid'),
+            "email": environ.get('authorisedUserEmail'),
+            "vmUsername": environ.get('vmUsername'),
+            "vmIpAddress": environ.get('vmIpAddress'),
+            "vmPassword": environ.get('vmPassword')
+        })
+    except Exception as e:
+        return jsonify({'error': 'There was an error setting up the log monitoring environment for the new component: ' + str(e)})
+    
+    return jsonify({'message': 'Component added successfully'})
+
+@app.route('/update-component/<int:cid>', methods=['PUT'])
+def update_component(cid):
+    component = Components.query.filter_by(cid=cid).first()
+    data = request.get_json()
+    component.update(**data)
+    db.session.commit()
+    return jsonify({'message': 'Component updated successfully'})
+
+@app.route('/delete-component/<int:cid>', methods=['DELETE'])
+def delete_component(cid):
+    component = Components.query.filter_by(cid=cid).first()
+    db.session.delete(component)
+    db.session.commit()
+    return jsonify({'message': 'Component deleted successfully'})
+
 
     
 if __name__ == '__main__':
