327f4d935d7ddad545428fac39a60d231e8b1f43
MikeyTheOng <michaelongningofficial@gmail.com>
2024-03-22 15:54:44 +0800

    Fixed notification pop-bar not moving with browser scroll

--- my-app/src/pages/components/navbar.tsx
+++ my-app/src/pages/components/navbar.tsx
@@ -45,6 +45,7 @@ const Sidebar = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [notifications, setNotifications] = useState<Notification[]>()
   const [names, setNames] = useState<Names>()
+  const notificationsDiv = document.getElementById("notifications-div") as HTMLElement;
 
   const fetchAllNotification = async () => {
     try {
@@ -145,60 +146,67 @@ const Sidebar = () => {
           </Link>
         ))}
       </div>
-      <Popover placement="right-end" shouldCloseOnBlur={true} className="ml-8">
-        <PopoverTrigger>
-          <div className="my-4 p-1">
-            <Badge content={unreadCount} color="danger">
-              <div className={`opacity-50 hover:opacity-100 hover:text-pri-500 
-                ${activeIndex === 100 
-                  ? "text-pri-500" 
-                  : "opacity-50 hover:opacity-100 hover:text-pri-500"}`}>
-                <IoNotificationsOutline size={25}/>
-              </div>
-            </Badge>
-          </div>    
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="px-1 py-2 h-fit w-80">
-            {notifications && notifications.length > 0 ? (
-              <>
-              <div className="font-bold">Unread Notifications</div>
-              <div className="overflow-y-auto max-h-80 my-5"> 
-                {notifications
-                  .filter(notification => !notification.isRead) 
-                  .map(notification => (
-                    <Link 
-                      key={notification.nid} 
-                      href={`/infraView?sid=${names?.[notification.cid.toString()]["sid"]}&cid=${notification.cid}`}
-                      onClick={() => markNotificationAsRead(notification.nid)}
-                    >
-                      <div className="pb-2 mb-3 border-b flex">
-                        <div className={`w-3 h-3 rounded-full mr-3 mt-[5px] ${notification.status === 'Critical' ? 'bg-reddish-100 border-2 border-reddish-200' :
-                          notification.status === 'Warning' ? 'bg-amberish-100 border-2 border-amberish-200' : 
-                          notification.status === 'Normal' ? 'bg-greenish-100 border-2 border-greenish-200' : ''}`}>
+      <div id="notifications-div">
+        <Popover 
+          placement="right-end" 
+          shouldCloseOnBlur={true} 
+          className="ml-8"
+          portalContainer={notificationsDiv}
+        >
+          <PopoverTrigger>
+            <div className="my-4 p-1">
+              <Badge content={unreadCount} color="danger">
+                <div className={`opacity-50 hover:opacity-100 hover:text-pri-500 
+                  ${activeIndex === 100 
+                    ? "text-pri-500" 
+                    : "opacity-50 hover:opacity-100 hover:text-pri-500"}`}>
+                  <IoNotificationsOutline size={25}/>
+                </div>
+              </Badge>
+            </div>    
+          </PopoverTrigger>
+          <PopoverContent className='z-100'>
+            <div className="px-1 py-2 h-fit w-80">
+              {notifications && notifications.length > 0 ? (
+                <>
+                <div className="font-bold">Unread Notifications</div>
+                <div className="overflow-y-auto max-h-80 my-5"> 
+                  {notifications
+                    .filter(notification => !notification.isRead) 
+                    .map(notification => (
+                      <Link 
+                        key={notification.nid} 
+                        href={`/infraView?sid=${names?.[notification.cid.toString()]["sid"]}&cid=${notification.cid}`}
+                        onClick={() => markNotificationAsRead(notification.nid)}
+                      >
+                        <div className="pb-2 mb-3 border-b flex">
+                          <div className={`w-3 h-3 rounded-full mr-3 mt-[5px] ${notification.status === 'Critical' ? 'bg-reddish-100 border-2 border-reddish-200' :
+                            notification.status === 'Warning' ? 'bg-amberish-100 border-2 border-amberish-200' : 
+                            notification.status === 'Normal' ? 'bg-greenish-100 border-2 border-greenish-200' : ''}`}>
+                          </div>
+                          <div className="w-11/12">
+                            <div className='font-bold'>{names?.[notification.cid.toString()]["sName"]} | {names?.[notification.cid.toString()]["mName"]} | {names?.[notification.cid.toString()]["cName"]}</div>
+                            <div className="break-all">{notification.reason}</div>
+                            <div className="text-tiny italic">{formatDate(notification.datetime)}</div>
+                          </div>
                         </div>
-                        <div className="w-11/12">
-                          <div className='font-bold'>{names?.[notification.cid.toString()]["sName"]} | {names?.[notification.cid.toString()]["mName"]} | {names?.[notification.cid.toString()]["cName"]}</div>
-                          <div className="break-all">{notification.reason}</div>
-                          <div className="text-tiny italic">{formatDate(notification.datetime)}</div>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-              </div>
-              <div className="text-center text-tiny flex hover:underline">
-                <Link href="/notification" className="flex flex-row items-center justify-center" onClick={() => setActiveIndex(100)}> 
-                  See all notifications
-                  <LuExternalLink className="ml-1"/>
-                </Link>
-              </div>
-              </>
-            ) : (
-              <div className="text-tiny flex justify-center items-center h-full py-3">No unread notifications.</div>
-            )}
-          </div>
-        </PopoverContent>
-      </Popover>
+                      </Link>
+                    ))}
+                </div>
+                <div className="text-center text-tiny flex hover:underline">
+                  <Link href="/notification" className="flex flex-row items-center justify-center" onClick={() => setActiveIndex(100)}> 
+                    See all notifications
+                    <LuExternalLink className="ml-1"/>
+                  </Link>
+                </div>
+                </>
+              ) : (
+                <div className="text-tiny flex justify-center items-center h-full py-3">No unread notifications.</div>
+              )}
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
       <button onClick={()=> signOut()}><div className="opacity-50 pb-6 hover:opacity-100 hover:text-pri-500"><AiOutlineLogout size={25}/></div></button>
     </div>
   );
--- my-app/src/pages/notification.tsx
+++ my-app/src/pages/notification.tsx
@@ -6,6 +6,7 @@ import Sidebar from "./components/navbar";
 import {Tabs, Tab, Chip, Tooltip} from "@nextui-org/react";
 import { RiErrorWarningLine } from "react-icons/ri";
 
+import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
 type Status = "Critical" | "Warning" | "Normal";
 interface Notification {
   nid: number,
@@ -169,9 +170,11 @@ export default function WorldView() {
 
   if(session && alerts && insights && names){
     return (
-      <main>
+      <main className="">
         <div className=" min-h-full flex flex-row">
-          <Sidebar/>
+          <div className="fixed z-50">
+            <Sidebar/>
+          </div>
           <div className="w-full pr-12 py-6 pl-28 h-full min-h-screen">
             <div id='top-menu' className="mb-4 flex flex-row justify-between">
               <h1 className='text-4xl font-bold text-pri-500 mt-1 pb-8 pt-2'>Notifications</h1>
@@ -181,6 +184,19 @@ export default function WorldView() {
               >
                 Read all
               </button>
+            </div>
+            <div id="test-popover">
+            <Popover placement="right">
+              <PopoverTrigger>
+                <Button>Open Popover</Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <div className="px-1 py-2">
+                  <div className="text-small font-bold">Popover Content</div>
+                  <div className="text-tiny">This is the popover content</div>
+                </div>
+              </PopoverContent>
+            </Popover>
             </div>
             <div className="flex w-full flex-col vh-100">
               <Tabs 
